13dbe4ddd56be79b7fd95ad2811bf2c16a2d3447
AndrexMed <giovanniandrex@gmail.com>
2023-07-10 22:41:42 -0500

    Dia2

--- Controllers/ActorController.cs
+++ Controllers/ActorController.cs
@@ -27,6 +27,33 @@ namespace IntroEF_Avanzado.Controllers
             return await _context.Actores.Where(a => a.Nombre == nombreActor).ToListAsync();
         }
 
+        [HttpGet("nombre/v2")]
+        public async Task<ActionResult<IEnumerable<Actor>>> GetV2(string nombreActor)
+        {
+            //Version2 - Contains
+            return await _context.Actores.Where(a => a.Nombre.Contains(nombreActor)).ToListAsync();
+        }
+
+        [HttpGet("FechaNacimiento/rango")]
+        public async Task<ActionResult<IEnumerable<Actor>>> GetFechaRango(DateTime inicio, DateTime fin)
+        {
+            return await _context.Actores
+                .Where(a => a.FechaNacimiento >= inicio && a.FechaNacimiento <= fin).ToListAsync();
+        }
+
+        //[HttpGet("{id:int")]
+        //public async Task<ActionResult<IEnumerable<Actor>>> GetActor(int id)
+        //{
+        //    var actor = await _context.Actores.FirstOrDefaultAsync(a => a.Id == id);
+
+        //    if (actor == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return Ok(actor);
+        //}
+
         [HttpGet]
         public async Task<ActionResult<IEnumerable<Actor>>> Get()
         {
